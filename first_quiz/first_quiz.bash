935cf08e90fa9a39575cd9fc65871ba61f7d3333
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-27 13:38:32 +0330

    :tada: init: init mini task and first_quiz

---
+++ first_quiz/first_quiz.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to /tmp
+cd /tmp
+
+# Create the directory
+mkdir first_quiz
+
+# Move into the directory
+cd first_quiz
+
+# Create a file named test1.txt
+touch test1.txt
+
+# List files with detailed information
+ls -l
